aa5e3449aa63e2dcc6016754f62e6c1537f7e59b
Richard Khonan <richardkhonan0797@gmail.com>
2019-10-04 12:08:26 +0700

    perbaikan Exercise 3

--- Exercise-3.js
+++ Exercise-3.js
@@ -1,18 +1,12 @@
-var nama = '';
-var peran = '';
+var nama = 'Mikael';
+var peran = 'tabib';
 
 if(nama === '' && peran === ''){
     console.log('Nama harus diisi');
-}else if(nama === 'Mikael' && peran === ''){
+}else if(peran === ''){
     console.log('Halo '+nama+', Pilih peranmu untuk memulai game!');
-}else if(nama === 'Nina' && peran === 'Ksatria'){
+}else{
     console.log('Selamat datang di Dunia Proxytia, '+nama);
-    console.log('Halo '+peran+' Nina, kamu dapat menyerang dengan senjatamu!');
-}else if(nama === 'Danu' && peran === 'Tabib'){
-    console.log('Selamat datang di Dunia Proxytia, '+nama);
-    console.log('Halo '+peran+' Danu, kamu akan membantu temanmu yang terluka.');
-}else if(nama === 'Zero' && peran === 'Penyihir'){
-    console.log('Selamat datang di Dunia Proxytia, '+nama);
-    console.log('Halo '+peran+' Zero, ciptakan keajaiban yang membantu kemenanganmu!');
+    console.log('Halo '+peran+' '+nama+', ciptakan keajaiban yang membantu kemenanganmu!');
 }
 
